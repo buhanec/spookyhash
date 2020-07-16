e25d9e78831ff552a26f79130a9a05342a1e2768
Alen Buhanec <alen.buhanec@gmail.com>
2020-07-16 06:57:35 +0100

    Add missing MANIFEST.in

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='spookyhash',
-    version='2.0.0',
+    version='2.0.1',
     author='Alen Buhanec',
     author_email='<alen.buhanec@gmail.com>',
     license='MIT',
