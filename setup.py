494abb5c9abbd97680319ad82010afd4ba2a32a6
Alen Buhanec <alen.buhanec@gmail.com>
2020-07-16 06:47:54 +0100

    Set Cython language_level to 3

--- setup.py
+++ setup.py
@@ -30,5 +30,6 @@ setup(
         Extension('spookyhash',
                   ['src/spookyhash.pyx', 'src/SpookyV2.cpp'],
                   language='c++'),
-    ], compiler_directives={'embedsignature': True}),
+    ], compiler_directives={'embedsignature': True,
+                            'language_level': 3}),
 )
